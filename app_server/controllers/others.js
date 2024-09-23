09e98a555a70cbe5617a8774c983948180e3b71c
kytice <kytice81@gmail.com>
2024-09-23 21:07:29 +0100

    Set up routing

---
+++ app_server/controllers/others.js
@@ -0,0 +1,7 @@
+/* GET home page */
+const about = function (req, res) {
+  res.render("index", { title: "About" });
+};
+module.exports = {
+  about,
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const ctrlLocations = require("../controllers/locations");
+const ctrlOthers = require("../controllers/others");
 
-const ctrlMain = require('../controllers/main'); 
-/* GET home page. */
-router.get('/', ctrlMain.index); 
+/* Locations pages */
+router.get("/", ctrlLocations.homelist);
+router.get("/location", ctrlLocations.locationInfo);
+router.get("/location/review/new", ctrlLocations.addReview);
+
+/* Other pages */
+router.get("/about", ctrlOthers.about);
 module.exports = router;
-
